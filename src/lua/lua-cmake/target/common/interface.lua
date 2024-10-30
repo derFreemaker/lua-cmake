127e98e1a5017ed4d04de796c04611ee209b5bd8
Freemaker <ralf.rose.oskar@gmail.com>
2024-10-30 14:10:44 +0100

    changed interface working

--- src/lua/lua-cmake/target/common/interface.lua
+++ src/lua/lua-cmake/target/common/interface.lua
@@ -6,13 +6,13 @@ local target_options = require("lua-cmake.target.options")
 ---@field hdrs string[] | nil
 ---@field srcs string[] | nil
 ---@field exclude_from_all boolean | nil
----@field options lua-cmake.target.options | nil
 ---
 ---@field deps string[] | nil
 
 local kind = "lua-cmake.target.interface"
 ---@class lua-cmake.target.interface : object
 ---@field config lua-cmake.target.interface.config
+---@field links string[]
 ---@overload fun(config: lua-cmake.target.interface.config) : lua-cmake.target.interface
 local interface = {}
 
@@ -32,25 +32,6 @@ function interface:__init(config)
         cmake.path_resolver.resolve_paths_implace(self.config.srcs)
     else
         self.config.srcs = {}
-    end
-
-    if not self.config.options then
-        self.config.options = {}
-    end
-
-    if self.config.options.precompile_headers then
-        for index, hdr_group in ipairs(self.config.options.precompile_headers) do
-            if type(hdr_group) == "table" then
-                cmake.path_resolver.resolve_paths_implace(hdr_group)
-            else
-                ---@cast hdr_group string
-                self.config.options.precompile_headers[index] = cmake.path_resolver.resolve_path(hdr_group)
-            end
-        end
-    end
-
-    if not self.config.options.link_libraries then
-        self.config.options.link_libraries = {}
     end
 
     if not self.config.deps then
@@ -90,61 +71,20 @@ function interface:__init(config)
         end,
         add_links = function(links)
             for _, link in ipairs(links) do
-                if utils.table.contains(self.config.options.link_libraries, link) then
+                if utils.table.contains(self.links, link) then
                     goto continue
                 end
 
-                table.insert(self.config.options.link_libraries, link)
+                table.insert(self.links, link)
 
                 ::continue::
             end
         end,
         on_dep = function(entry)
-            entry.add_links({ self.config.name })
+            entry.add_hdrs(self.config.hdrs)
+            entry.add_srcs(self.config.srcs)
+            entry.add_links(self.links)
         end
-    })
-
-    cmake.generator.add_action({
-        kind = kind,
-        ---@param context lua-cmake.target.interface.config
-        func = function(builder, context)
-            local name = utils.make_name_cmake_conform(context.name)
-            builder:append_line("add_library(", name, " INTERFACE")
-
-            if context.exclude_from_all then
-                builder:append_indent()
-                    :append_line("EXCLUDE_FROM_ALL")
-            end
-
-            for _, hdr in ipairs(context.hdrs) do
-                builder:append_indent()
-                    :append_line("\"", hdr, "\"")
-            end
-
-            for _, src in ipairs(context.srcs) do
-                builder:append_indent()
-                    :append_line("\"", src, "\"")
-            end
-
-            builder:append_line(")")
-
-            if not utils.is_name_cmake_conform(context.name) then
-                builder:append_line("add_library(", context.name, " ALIAS ", name, ")")
-            end
-
-            cmake.generator.add_action({
-                kind = kind .. ".options",
-                ---@param options_context { name: string, options: lua-cmake.target.options }
-                func = function(options_builder, options_context)
-                    target_options(options_builder, options_context.name, options_context.options, true)
-                end,
-                context = {
-                    name = name,
-                    options = context.options
-                }
-            })
-        end,
-        context = self.config
     })
 end
 
