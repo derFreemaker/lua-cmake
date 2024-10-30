2f4b28bc8cbb64542722e5a625444502c8cd7a20
Freemaker <ralf.rose.oskar@gmail.com>
2024-10-30 18:35:05 +0100

    fixed

--- bin/lua-cmake.bat
+++ bin/lua-cmake.bat
@@ -4,4 +4,4 @@ set "SCRIPT_DIR=%~dp0"
 set "PARENT_DIR=%SCRIPT_DIR:~0,-1%"
 for %%a in ("%PARENT_DIR%") do set "PARENT_DIR=%%~dpa"
 
-lua %LUA_CMAKE_DIR%/src/main.lua %PARENT_DIR% %*
+lua %PARENT_DIR%/src/main.lua %PARENT_DIR% %*
